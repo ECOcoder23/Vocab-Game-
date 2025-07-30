676a313e85a0617b0c9b9dce2f58338c8b28d9f7
ECOcoder23 <eobiozor@luc.edu>
2025-07-30 03:44:51 +0000

    Another draft

--- workspace.py
+++ workspace.py
@@ -1,11 +1,12 @@
-path_to_file = "/workspaces/Vocab-Game-"
+path_to_file = "/workspaces/Vocab-Game-/"
 file = "vocab.txt"
 while True:
     menu_options = input("1 - Create new friend record\n2 - Search for a friend\n3 - Run reports\n4 - Exit\n: ")
     numeric = int(menu_options)
     if numeric == 4:
         prompt_1 = "Application closed, Goodbye"
-        with open(path_to_file + file, "wt") as record:
-            record.write(prompt_1)
+        record = open(path_to_file + file, "wt")
+        record.write(prompt_1)
+        record.close()
         print("Application closed, Goodbye")
         break 
