d1492abce84b5391de7aa67d9cb7cf19b1fcbc31
ECOcoder23 <eobiozor@luc.edu>
2025-07-31 18:42:51 +0000

    another draft

--- workspace.py
+++ workspace.py
@@ -1,12 +1,23 @@
 path_to_file = "/workspaces/Vocab-Game-/"
 file = "vocab.txt"
 while True:
-    menu_options = input("1 - Create new friend record\n2 - Search for a friend\n3 - Run reports\n4 - Exit\n: ")
+    prompt_1 = "Application closed, Goodbye"
+    menu_options = input("1 - Create new friend record\n2 - Search for a friend\n3 - Run reports\n4 - Exit\n-> ")
     numeric = int(menu_options)
-    if numeric == 4:
-        prompt_1 = "Application closed, Goodbye"
+    if numeric == 2: 
+        load = open(path_to_file + file, "r")
+        for line in load:
+            print(f"Data found: {line}")
+    elif numeric == 4:
         record = open(path_to_file + file, "wt")
         record.write(prompt_1)
         record.close()
-        print("Application closed, Goodbye")
+        print(prompt_1)
         break 
+
+class Thing:
+
+    def __init__(self, does_exist, is_real):
+        self.does_exist = does_exist
+        self.is_real = is_real
+
